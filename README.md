ab9573c58f197c00fea412279cb402dd845da39e
Fiona <fionnaagorrie@gmail.com>
2019-10-16 15:04:16 -0600

    core front-end features added, initial readme added

---
+++ README.md
@@ -0,0 +1,68 @@
+**Note: this readme is still under construction
+
+Name:
+ClimApp
+
+Description:
+An App for users to find climbing routes that match their experience level, equiptment capability, and by rating. 
+
+First edition to include all the kernal elements required for the Greenfield/MVP project:
+
+A brand-new codebase, never worked on before
+A Flask Backend 
+A Vue FrontEnd
+With at LEAST 5 components
+Parent components should pass Props to child components
+A Postgres DB (We’ll start with SQLite3 and then migrate)
+External API calls
+Internal  API calls
+A comprehensive README.md file
+
+The Greenfield project MAY incorporate the following features:
+Authentication
+New technologies not yet discussed in this class-- eg:
+Vuex (Store management)
+Drag and Drop
+D3.js (Charts and graphs)
+
+Installation:
+
+Clone the repo using git clone <https://github.com/Fiona-Gorrie/ClimbApp.git>
+
+To get the server up and running, do the following from the project's root:
+cd Server
+pipenv install 
+pipenv run python main.py
+
+To get Vue running, do the following from the project's root in an additional terminal screen:
+cd client
+npm install
+npm run serve
+
+Roadmap:
+Future editions may include:
+
+Weather information for the climbing area
+
+Ability to search multiple types of route: Rock, Bouldering, Ice, Mixed. Each type having it's own dficulty rating system and required gear.
+
+User Signup
+
+State if you are open to contributions and what your requirements are for accepting them:
+
+**
+You can also document commands to lint the code or run tests. These steps help to ensure high code quality and reduce the likelihood that the changes inadvertently break something. Having instructions for running tests is especially helpful if it requires external setup, such as starting a Selenium server for testing in a browser.
+**
+
+Authors and acknowledgment
+
+Gratitude to the instructor, Phil Gonzalez, and TA's (Heath Naylor, Hamp Goodwin, Kyle Misencik, Chris Vincent) of Devetry's Code Forward Full Stack course for their time and support throughoutt the course and continued mentorship during our capstone projects and beyond into our hopeful employment as Jr dev's! 
+
+**
+License
+
+For open source projects, say how it is licensed.
+Project status
+
+If you have run out of energy or time for your project, put a note at the top of the README saying that development has slowed down or stopped completely. Someone may choose to fork your project or volunteer to step in as a maintainer or owner, allowing your project to keep going. You can also make an explicit request for maintainers.
+**
--- client/src/components/ClimbApp.vue
+++ client/src/components/ClimbApp.vue
@@ -1,7 +1,105 @@
 <template>
   <div class="hello">
     <h1>{{ title }}</h1>
+
+    <h1>Climb Type</h1>
+
+    <form action="">
+      <input type="checkbox" name="Trad" value="Trad">Trad<br>
+      <input type="checkbox" name="Sport" value="Sport">Sport<br>
+      <input type="checkbox" name="Some Gear" value="Some Gear">Some Gear<br>
+      <input type="checkbox" name="Top Rope" value="Top Rope" checked>Top Rope<br><br>
+    </form>
+
+    <h2>From Grade</h2>
+    <select>
+      <option value="3rd">3rd</option>
+      <option value="4th">4th</option>
+      <option value="5.0">5.0</option>
+      <option value="5.1">5.1</option>
+      <option value="5.2">5.2</option>
+      <option value="5.3">5.3</option>
+      <option value="5.4">5.4</option>
+      <option value="5.5">5.5</option>
+      <option value="5.6">5.6</option>
+      <option value="5.7">5.7</option>
+      <option value="5.8">5.8</option>
+      <option value="5.9">5.9</option>
+      <option value="5.10a">5.10a</option>
+      <option value="5.10b">5.10b</option>
+      <option value="5.10c">5.10c</option>
+      <option value="5.10d">5.10d</option>
+      <option value="5.11a">5.11a</option>
+      <option value="5.11b">5.11b</option>
+      <option value="5.11c">5.11c</option>
+      <option value="5.11d">5.11d</option>
+      <option value="5.12a">5.12a</option>
+      <option value="5.12b">5.12b</option>
+      <option value="5.12c">5.12c</option>
+      <option value="5.12d">5.12d</option>
+      <option value="5.13a">5.13a</option>
+      <option value="5.13b">5.13b</option>
+      <option value="5.13c">5.13c</option>
+      <option value="5.13d">5.13d</option>
+      <option value="5.14a">5.14a</option>
+      <option value="5.14b">5.14b</option>
+      <option value="5.14c">5.14c</option>
+      <option value="5.14d">5.14d</option>
+      <option value="5.15a">5.15a</option>
+      <option value="5.15b">5.15b</option>
+      <option value="5.15c">5.15c</option>
+      <option value="5.15d">5.15d</option>            
+    </select>
+
+    <h2>To Grade</h2>
+    <select>
+      <option value="3rd">3rd</option>
+      <option value="4th">4th</option>
+      <option value="5.0">5.0</option>
+      <option value="5.1">5.1</option>
+      <option value="5.2">5.2</option>
+      <option value="5.3">5.3</option>
+      <option value="5.4">5.4</option>
+      <option value="5.5">5.5</option>
+      <option value="5.6">5.6</option>
+      <option value="5.7">5.7</option>
+      <option value="5.8">5.8</option>
+      <option value="5.9">5.9</option>
+      <option value="5.10a">5.10a</option>
+      <option value="5.10b">5.10b</option>
+      <option value="5.10c">5.10c</option>
+      <option value="5.10d">5.10d</option>
+      <option value="5.11a">5.11a</option>
+      <option value="5.11b">5.11b</option>
+      <option value="5.11c">5.11c</option>
+      <option value="5.11d">5.11d</option>
+      <option value="5.12a">5.12a</option>
+      <option value="5.12b">5.12b</option>
+      <option value="5.12c">5.12c</option>
+      <option value="5.12d">5.12d</option>
+      <option value="5.13a">5.13a</option>
+      <option value="5.13b">5.13b</option>
+      <option value="5.13c">5.13c</option>
+      <option value="5.13d">5.13d</option>
+      <option value="5.14a">5.14a</option>
+      <option value="5.14b">5.14b</option>
+      <option value="5.14c">5.14c</option>
+      <option value="5.14d">5.14d</option>
+      <option value="5.15a">5.15a</option>
+      <option value="5.15b">5.15b</option>
+      <option value="5.15c">5.15c</option>
+      <option value="5.15d">5.15d</option>            
+    </select>
+
+    <h2>Rating</h2>
+  
+    &star;
+    &star;
+    &star;
+    &star;
+
   </div>
+  
 </template>
 
 <script>
